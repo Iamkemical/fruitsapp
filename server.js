cd7c2d5ee0a7cc4619a54e24e8b14fdf6e60c823
Gabriel Isaac <iamkemical1@gmail.com>
2024-10-26 00:04:52 +0100

    Added full text search

--- server.js
+++ server.js
@@ -1,58 +1,55 @@
-require('dotenv').config({path: `${process.cwd()}/.env`});
-const express = require('express');
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const express = require("express");
 
 const app = express();
 
-const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, Op } = require("sequelize");
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config');
+const env = process.env.NODE_ENV || "development";
+const config = require("./config/config");
 const sequelize = new Sequelize(config[env]);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-      status: 'Success',
-      message: `FruitsApi ${process.env.NODE_ENV} environment`,
-      data: null
+    status: "Success",
+    message: `FruitsApi ${process.env.NODE_ENV} environment`,
+    data: null,
   });
 });
 
+app.get("/api/fruit", async (req, res) => {
+  const { name, color, season, createdAt } = req.query;
 
-app.get('/api/fruit/getfruits', async (req, res) => {
-    const { name, color, season, createdAt} = req.query;
-    
-    let baseQuery = 'SELECT color, name, price, season, "createdAt" FROM public.fruits WHERE "createdAt" IS NOT null';
-    if(name) {
-        baseQuery += ' AND name =$name';
-    }
-    if(color) {
-        baseQuery += ' AND color =$color';
-    }
-    if(season) {
-        baseQuery += ' AND season =$season';
-    }
-    const [result, metadata] = await sequelize.query(baseQuery, {
-    bind: {
-        name: name,
-        color: color,
-        season: season
-    }
-    });
-    
-    return res.status(200).json(
-        {
-            status: 'Success',
-            message: 'Fruits fetched successfully',
-            data: result
-        }
-    )
+  let baseQuery = `SELECT color, name, price, season, "createdAt" FROM public.fruits WHERE "createdAt" IS NOT null`;
+  const replacements = {};
+  if (name) {
+    baseQuery += ` AND to_tsvector('english', name) @@ to_tsquery('english', :name || ':*')`;
+    replacements.name = name;
+  }
+  if (color) {
+    baseQuery += ` AND to_tsvector('english', color) @@ to_tsquery('english', :color || ':*')`;
+    replacements.color = color;
+  }
+  if (season) {
+    baseQuery += ` AND to_tsvector('english', season) @@ to_tsquery('english', :season || ':*')`;
+    replacements.season = season;
+  }
+  const [result, metadata] = await sequelize.query(baseQuery, {
+    replacements,
+  });
+
+  return res.status(200).json({
+    status: "Success",
+    message: "Fruits fetched successfully",
+    data: result,
+  });
 });
 
 const PORT = process.env.APP_PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log('Server up and running on port', PORT);
+  console.log("Server up and running on port", PORT);
 });
